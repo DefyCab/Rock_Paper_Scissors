a6f25de5616d10bf7c65fcb5d46ad4f6f3148e04
DefyCab <love.bellinder@gmail.com>
2021-12-04 18:48:21 +0100

    Set up project structure clean up and remove unnessecary file add cypress as dependecy

---
+++ src/components/Ai.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import AiResponse from '../modules/AiResponse'
+
+import React from 'react'
+
+const Ai = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Ai
---
+++ src/modules/AiResponse.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AiResponse = () => {}
+
+const randomNumber = (roof) => {
+
+  return (
+
+    Math.floor(Math.random() * roof)
+
+  )
+}
+
+export default AiResponse
