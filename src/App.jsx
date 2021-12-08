b5fd0a5ece8483df2c03bc1db4a6d09e408ccf15
DefyCab <love.bellinder@gmail.com>
2021-12-09 00:12:43 +0100

    refactor back to App.jsx avoid creating modules not being used can refactor out later

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
-import User from './components/User'
-import Ai from './components/Ai'
+// import User from './components/User'
+// import Ai from './components/Ai'
 
 const App = () => {
   return (
     <>
       <div>
-        <User />
-        <Ai />
+        <button data-cy="rock-button">Rock</button>
+        <button data-cy="paper-button">Paper</button>
+        <button data-cy="scissors-button">Scissors</button>
+        
+        <image data-cy="rock-icon"></image>
+        <image data-cy="paper-icon"></image>
+        <image data-cy="scissors-icon"></image>
       </div>
     </>
   )
