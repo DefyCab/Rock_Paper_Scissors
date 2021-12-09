6609d5d0322533c10661db6be44ce517f44f9190
DefyCab <love.bellinder@gmail.com>
2021-12-09 19:30:45 +0100

    added color to buttons and amplified size

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,78 @@
-import React, { useState } from 'react'
-// import User from './components/User'
-// import Ai from './components/Ai'
+import React, { useState } from "react";
+import { Button } from "semantic-ui-react"
+import "semantic-ui-css/semantic.min.css"
 
 const App = () => {
-  const [userNumber, setUserNumber] = useState(4)
+  const [userNumber, setUserNumber] = useState(4);
 
   const handleClick = (number) => {
-    setUserNumber(number)
-  }
+    setUserNumber(number);
+  };
 
-  const [randomNumber, setRandomNumber] = useState()
+  const [randomNumber, setRandomNumber] = useState();
 
   const alsoClick = (max) => {
-    setRandomNumber(Math.floor(Math.random() * max))
-  }
+    setRandomNumber(Math.floor(Math.random() * max));
+  };
 
   const gameEngine = () => {
-    if (userNumber === 0 && randomNumber === 1) return 'Computer win!'
-    if (userNumber === 0 && randomNumber === 2) return 'You win!'
-    if (userNumber === 1 && randomNumber === 0) return 'You win!'
-    if (userNumber === 1 && randomNumber === 2) return 'Computer win!'
-    if (userNumber === 2 && randomNumber === 1) return 'You win!'
-    if (userNumber === 2 && randomNumber === 0) return 'Computer win!'
-    else if (userNumber === 4) return ''
-    else return "It's a tie!"
-  }
+    if (userNumber === 0 && randomNumber === 1) return "Computer win!";
+    if (userNumber === 0 && randomNumber === 2) return "You win!";
+    if (userNumber === 1 && randomNumber === 0) return "You win!";
+    if (userNumber === 1 && randomNumber === 2) return "Computer win!";
+    if (userNumber === 2 && randomNumber === 1) return "You win!";
+    if (userNumber === 2 && randomNumber === 0) return "Computer win!";
+    else if (userNumber === 4) return "";
+    else return "It's a tie!";
+  };
 
   const userChoice = () => {
-    if (userNumber === 0)
-    return "Rock"
-    if (userNumber === 1)
-    return "Paper"
-    if (userNumber === 2)
-    return "Scissors"
-    else
-    return ""
-  }
+    if (userNumber === 0) return "Rock";
+    if (userNumber === 1) return "Paper";
+    if (userNumber === 2) return "Scissors";
+    else return "";
+  };
 
   const compChoice = () => {
-    if (randomNumber === 0)
-    return "Rock"
-    if (randomNumber === 1)
-    return "Paper"
-    if (randomNumber === 2)
-    return "Scissors"
-    else
-    return ""
-  }
+    if (randomNumber === 0) return "Rock";
+    if (randomNumber === 1) return "Paper";
+    if (randomNumber === 2) return "Scissors";
+    else return "";
+  };
 
   return (
     <>
       <div>
-        <button
+        <Button color="teal"
+          size="massive"
           data-cy="rock-button"
           onClick={() => {
-            handleClick(0)
-            alsoClick(3)
+            handleClick(0);
+            alsoClick(3);
           }}
         >
           Rock
-        </button>
-        <button
+        </Button>
+        <Button color="green"
+          size="massive"
           data-cy="paper-button"
           onClick={() => {
-            handleClick(1)
-            alsoClick(3)
+            handleClick(1);
+            alsoClick(3);
           }}
         >
           Paper
-        </button>
-        <button
+        </Button>
+        <Button color="purple"
+          size="massive"
           data-cy="scissors-button"
           onClick={() => {
-            handleClick(2)
-            alsoClick(3)
+            handleClick(2);
+            alsoClick(3);
           }}
         >
           Scissors
-        </button>
+        </Button>
 
         <image data-cy="rock-icon"></image>
         <image data-cy="paper-icon"></image>
@@ -89,7 +84,7 @@ const App = () => {
         <p data-cy="result-output">Result: {`${gameEngine()}`} </p>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/modules/RandomNumber.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const RandomNumber = (roof) => {
-  return Math.floor(Math.random() * roof)
-}
-
-export default RandomNumber
