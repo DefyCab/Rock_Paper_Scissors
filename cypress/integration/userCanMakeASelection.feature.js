3d2f99dd2cca92268ca7c8855af1a4660d989aaa
DefyCab <love.bellinder@gmail.com>
2021-12-09 01:16:51 +0100

    struggle with cypress test syntax adds function so the ai random aciton updates every time the app renders

--- cypress/integration/userCanMakeASelection.feature.js
+++ cypress/integration/userCanMakeASelection.feature.js
@@ -3,15 +3,16 @@ describe('User can make a selection between rock, paper and scissors', () => {
   beforeEach(() => {
     cy.visit('http://localhost:3000')
   })
-  it ('is expected to return value of 0', () => {
-    cy.get('[data-cy=rock-button]').click().should('have.value', 0)
+  it('is expected to return value of 0', () => {
+    cy.get('[data-cy=rock-button]').click()
+    expect(userNumber).to.eq(0)
   })
-  it ('is expected to return value of 1', () => {
-    cy.get('[data-cy=paper-button]').click().should('have.value', 1)
+  it('is expected to return value of 1', () => {
+    cy.get('[data-cy=paper-button]').click()
+    expect(userNumber).to.eq(1)
   })
-  it ('is expected to return value of 2', () => {
-    cy.get('[data-cy=scissors-button]').click().should('have.value', 2)
+  it('is expected to return value of 2', () => {
+    cy.get('[data-cy=scissors-button]').click()
+    expect(userNumber).to.eq(2)
   })
 })
-
-  
--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import User from './components/User'
 // import Ai from './components/Ai'
 
 const App = () => {
+
+const [userNumber, setUserNumber] = useState()
+
+  const handleClick = (number) => {
+    setUserNumber(number)
+  }
+
+  const [randomNumber, setRandomNumber] = useState()
+
+  const alsoClick = (max) => {
+    setRandomNumber (Math.floor(Math.random() * max))
+  }
+
   return (
     <>
       <div>
-        <button data-cy="rock-button">Rock</button>
-        <button data-cy="paper-button">Paper</button>
-        <button data-cy="scissors-button">Scissors</button>
+        <button data-cy="rock-button" onClick={() => {
+          handleClick(0)
+          alsoClick(3)}}>Rock</button>
+        <button data-cy="paper-button" onClick={() => {
+          handleClick(1)
+          alsoClick(3)}}>Paper</button>
+        <button data-cy="scissors-button" onClick={() => {
+          handleClick(2)
+          alsoClick(3)}}>Scissors</button>
         
         <image data-cy="rock-icon"></image>
         <image data-cy="paper-icon"></image>
         <image data-cy="scissors-icon"></image>
+
+        <p> User selection: {userNumber}</p>
+        <p> Ai number: {randomNumber}</p>
       </div>
     </>
   )
+
 }
 
 export default App
